4c8a3a6ef2366e6cc5d00ec08c79e8714fb2f51c
SuakRolando <s21910305@student.unklab.ac.id>
2022-07-06 22:28:48 +0800

    membuat struktur folder

--- silonbog-testing/src/App.js
+++ silonbog-testing/src/App.js
@@ -1,18 +1,13 @@
 import React from "react";
 import "typeface-poppins";
 import "./App.css";
+import { SignIn } from "./pages";
+
+
 const App = () => {
   return (
-    <div className="header">
-      <button
-        type="button"
-        className="btn btn-primary"
-      >
-        Primary
-      </button>
-      <h1>Bold</h1>
-      <p>pppp</p>
-      <i className="bi bi-alarm-fill text-warning" style={{ fontSize: 50 }}></i>
+    <div className="content">
+      <SignIn/>      
     </div>
   );
 };
---
+++ silonbog-testing/src/pages/SignIn/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Styles from '../../assets/Styles/SignIn.module.css';
+
+const SignIn = () => {
+  return (
+    <div className={Styles.container}>SignIn</div>
+  )
+}
+
+export default SignIn
