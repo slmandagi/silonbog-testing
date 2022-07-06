fcf06afaefe7cce6ec30f17fda90f685ddf3ff99
slmandagi <92778838+slmandagi@users.noreply.github.com>
2022-07-06 20:36:57 +0800

    menambahkan beberapa import package

--- silonbog-testing/src/App.js
+++ silonbog-testing/src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
-
+import "typeface-poppins";
+import "./App.css";
 const App = () => {
   return (
-    <div>
-      <button type="button" className="btn btn-primary">
+    <div className="header">
+      <button
+        type="button"
+        className="btn btn-primary"
+        style={{ fontFamily: "poppins", fontWeight: 100 }}
+      >
         Primary
       </button>
       <i className="bi bi-alarm-fill text-warning" style={{ fontSize: 50 }}></i>
