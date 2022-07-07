0a0323f0b4f65d22c70f3f2c2e94aa630ee6905e
SuakRolando <s21910305@student.unklab.ac.id>
2022-07-07 08:01:11 +0800

    membuat page sign in

--- silonbog-testing/src/App.js
+++ silonbog-testing/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "typeface-poppins";
-import "./App.css";
+import Styles from "./assets/Styles/App.module.css";
 import { SignIn } from "./pages";
 
 
 const App = () => {
   return (
-    <div className="content">
+    <div className={`${Styles.content}`}>
       <SignIn/>      
     </div>
   );
--- silonbog-testing/src/pages/SignIn/index.js
+++ silonbog-testing/src/pages/SignIn/index.js
@@ -3,7 +3,13 @@ import Styles from '../../assets/Styles/SignIn.module.css';
 
 const SignIn = () => {
   return (
-    <div className={Styles.container}>SignIn</div>
+    <div className={Styles.container}>
+      <div>
+        <p className={Styles.txtWelcome}>Selamat Datang di</p>
+        <h1 className={Styles.txtTitle}>SILONBOG</h1>
+        <h6 className={Styles.txtTitleExpansion}>Sistem Layanan Online Biro Organisasi</h6>
+      </div>
+    </div>
   )
 }
 
